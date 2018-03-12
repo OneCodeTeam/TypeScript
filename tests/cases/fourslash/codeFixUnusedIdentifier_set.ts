e1f108b6d4d0910ef231c1495077a69aec3c4958
Andy Hanson <anhans@microsoft.com>
2018-03-12 08:31:24 -0700

    fixUnusedIdentifier: Don't remove setter parameter

---
+++ tests/cases/fourslash/codeFixUnusedIdentifier_set.ts
@@ -0,0 +1,14 @@
+/// <reference path='fourslash.ts' />
+
+// @noUnusedLocals: true
+// @noUnusedParameters: true
+
+////class C {
+////    set x(value: number) {}
+////}
+
+verify.codeFix({
+    description: "",
+    newFileContent: "",
+});
+
